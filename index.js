53d0137cea62d5c9302acf2c7d4a5da76b13aa8e
Daniel Husar <dano.husar@gmail.com>
2014-08-25 17:02:43 +0100

    Avoid pushing untranspiled code in case of an error

--- index.js
+++ index.js
@@ -2,48 +2,45 @@
 
 var through = require('through2');
 var gutil = require('gulp-util');
-var applySourceMap = require('vinyl-sourcemaps-apply');
 var pleeease = require('pleeease');
+var applySourceMap = require('vinyl-sourcemaps-apply');
 
 module.exports = function (opts) {
   opts = opts ? opts : {};
 
   return through.obj(function (file, enc, cb) {
     if (file.isNull()) {
-      this.push(file);
-      return cb();
+      cb(null, file);
+      return;
     }
 
     if (file.isStream()) {
-      this.emit('error', new gutil.PluginError('gulp-pleeease', 'Streaming not supported'));
-      return cb();
+      cb(new gutil.PluginError('gulp-pleeease', 'Streaming not supported'));
+      return;
     }
 
     try {
       if (file.sourceMap) {
-        opts.sourcemaps = {};
-
-        if (file.base && file.path) {
-          opts.sourcemaps.from = file.relative;
-        } else {
-          opts.sourcemaps.from = file.path;
-        }
-        opts.sourcemaps.map = { annotation: false };
+        opts.sourcemaps = {
+          from : file.relative || file.path,
+          map : {
+            annotation: false
+          }
+        };
       }
 
       var result = pleeease.process(file.contents.toString(), opts);
       file.contents = new Buffer(result.css || result);
-
-      // apply source map to the chain
-      if (file.sourceMap && result.map && result.map.toString) {
+      if (file.sourceMap && result.map) {
         applySourceMap(file, result.map.toString());
       }
 
+      cb(null, file);
+
     } catch (err) {
+      cb(new gutil.PluginError('gulp-pleeease', err, {fileName: file.path}));
       this.emit('error', new gutil.PluginError('gulp-pleeease', err));
     }
 
-    this.push(file);
-    cb();
   });
 };
