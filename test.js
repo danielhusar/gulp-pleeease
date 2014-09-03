0d17a72d2a197cd797f567182285117545d20896
Daniel <dano.husar@gmail.com>
2014-09-03 22:19:50 +0100

    bump dependencies

--- test.js
+++ test.js
@@ -11,7 +11,7 @@ it('It should process data', function (cb) {
     },
   });
   stream.on('data', function (file) {
-    assert.equal(file.contents.toString(), '@media all{body{color:red;}a{color:blue;}}');
+    assert.equal(file.contents.toString(), '@media all{body{color:red}a{color:blue}}');
     cb();
   });
 
@@ -36,10 +36,10 @@ it('It should generate source maps', function (cb) {
   init.pipe(stream).pipe(write);
 
   write.on('data', function (file) {
-    assert.equal(file.sourceMap.mappings, 'AAAA,WAAW,KAAK,WAAU,AAAc,CAAb,CAAe,YAAW,CAAC,CAA1B');
-    assert(file.contents.toString().match('@media all{body{color:red;}a{color:blue;}}'));
+    assert.equal(file.sourceMap.mappings, 'AAAA,WAAW,KAAK,UAAU,AAAc,CAAb,CAAe,WAAW,CAAC,CAA1B');
+    assert(file.contents.toString().match('@media all{body{color:red}a{color:blue}}'));
     assert(file.contents.toString().match('sourceMappingURL=data:application/json;base64'));
-    assert(file.contents.toString().match('eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInN0eWxlLmNzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxXQUFXLEtBQUssV0FBVSxBQUFjLENBQWIsQ0FBZSxZQUFXLENBQUMsQ0FBMUIiLCJmaWxlIjoic3R5bGUuY3NzIiwic291cmNlc0NvbnRlbnQiOlsiQG1lZGlhIGFsbHtib2R5e2NvbG9yOnJlZDt9fSBAbWVkaWEgYWxse2F7Y29sb3I6Ymx1ZTt9fSJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ'));
+    assert(file.contents.toString().match('eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInN0eWxlLmNzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxXQUFXLEtBQUssVUFBVSxBQUFjLENBQWIsQ0FBZSxXQUFXLENBQUMsQ0FBMUIiLCJmaWxlIjoic3R5bGUuY3NzIiwic291cmNlc0NvbnRlbnQiOlsiQG1lZGlhIGFsbHtib2R5e2NvbG9yOnJlZDt9fSBAbWVkaWEgYWxse2F7Y29sb3I6Ymx1ZTt9fSJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ'));
     cb();
   });
 
